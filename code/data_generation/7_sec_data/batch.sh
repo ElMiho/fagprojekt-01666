de86fef7588920c8f9e2edecc434922bdf8bb9b6
ElMiho <mikael.hjermitslev@gmail.com>
2023-03-03 20:49:46 +0100

    Add 7 sec data and missing 0 deg data for 0.1 sec

---
+++ code/data_generation/7_sec_data/batch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh 
+#BSUB -q hpc
+#BSUB -J 7_sec_test
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -W 72:00
+#BSUB -u s214753@dtu.dk
+#BSUB -B 
+#BSUB -N 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
+
+module load mathematica/12.2.0
+wolframscript -config WOLFRAMSCRIPT_KERNELPATH="/appl/mathematica/12.2.0/Executables/WolframKernel"
+wolframscript -file generate_test_data.wls 1 5
+wolframscript -file generate_test_data.wls 5 10
+wolframscript -file generate_test_data.wls 10 15
+wolframscript -file generate_test_data.wls 15 20
+wolframscript -file generate_test_data.wls 20 25
+wolframscript -file generate_test_data.wls 25 30
+wolframscript -file generate_test_data.wls 30 35
+wolframscript -file generate_test_data.wls 35 40
+wolframscript -file generate_test_data.wls 40 45
