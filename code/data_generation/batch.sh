eaffd04daeb8c17520ec9b31dce55d4975b97188
ElMiho <mikael.hjermitslev@gmail.com>
2023-03-03 20:50:38 +0100

    Small testing with generation of test data and batch script

--- code/data_generation/batch.sh
+++ code/data_generation/batch.sh
@@ -2,7 +2,7 @@
 #BSUB -q hpc
 #BSUB -J test_batch
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "rusage[mem=4GB]"
 #BSUB -W 24:00
 #BSUB -u s214753@dtu.dk
 #BSUB -B 
@@ -12,4 +12,12 @@
 
 module load mathematica/12.2.0
 wolframscript -config WOLFRAMSCRIPT_KERNELPATH="/appl/mathematica/12.2.0/Executables/WolframKernel"
-wolframscript -file new_generate_polynomials.wls
+wolframscript -file generate_test_data.wls 1 5
+wolframscript -file generate_test_data.wls 5 10
+wolframscript -file generate_test_data.wls 10 15
+wolframscript -file generate_test_data.wls 15 20
+wolframscript -file generate_test_data.wls 20 25
+wolframscript -file generate_test_data.wls 25 30
+wolframscript -file generate_test_data.wls 30 35
+wolframscript -file generate_test_data.wls 35 40
+wolframscript -file generate_test_data.wls 40 45
