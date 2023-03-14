4001e78006709a1c40480c32e4d351484ef85f64
ElMiho <mikael.hjermitslev@gmail.com>
2023-03-14 12:34:58 +0100

    Work from 14/3-2023

--- code/data_generation/structured_generation_of_polynomials.py
+++ code/data_generation/structured_generation_of_polynomials.py
@@ -1,14 +1,17 @@
 import math
 
-def integer_to_list(n, sum_deg, base):
-    l = []
+
+
+def integer_to_list(n, numerator, denominator, base):
+    l = [numerator, denominator]
+    sum_deg = numerator + denominator
     for i in range(1, sum_deg + 1):
         var = n % base**i
         l.append(var // (base**(i-1)))
         n = n - var
     return l
 
-print(integer_to_list(1 + 2 * 45 + 3 * 45**2, 6, 45))
+print(integer_to_list(1 + 2 * 45 + 3 * 45**2, 2, 4, 45))
 
 total = 45**6
 needed = 10**6
