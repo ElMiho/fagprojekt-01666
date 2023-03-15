f4c1e50c814fd6eeac433cc1dbad987995208e74
ElMiho <mikael.hjermitslev@gmail.com>
2023-03-15 22:57:37 +0100

    Start of sampling batch job

---
+++ code/sampling/sampling.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+#BSUB -q hpc
+#BSUB -J sampling
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 12:00
+#BSUB -u s214753@dtu.dk
+#BSUB -B 
+#BSUB -N 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err
+
+module load matplotlib/3.7.0-numpy-1.24.2-python-3.11.2
+python3 random_sampling.py
