cb8fbab5188c370f4aa36dcb37868b5585f87f86
ElMiho <mikael.hjermitslev@gmail.com>
2023-03-30 12:51:33 +0200

    Update sample parameters (code and file)
    Also added a rough time estimate

--- code/data_analysis/generate_sample_parameters.py
+++ code/data_analysis/generate_sample_parameters.py
@@ -6,7 +6,8 @@ import numpy as np
 sample_size = np.loadtxt('SampleSize.txt', delimiter=',')
 sample_parameters = []
 
-number_of_tokens = 34
+number_of_tokens_num = 39
+number_of_tokens_den = 34
 total_samples = 40*10**6
 categories = 44
 n = total_samples//categories
@@ -15,7 +16,7 @@ for i in range(44):
     den = sample_size[i,1]
     sample_factor = sample_size[i,2]
     number_of_samples = n*sample_factor
-    omega = number_of_tokens**(num+den)     #Change according to different bases
+    omega = number_of_tokens_num**num*number_of_tokens_den**den     #Change according to different bases
     sample_parameters.append((num,den,number_of_samples,omega))
 sample_parameters = np.asarray(sample_parameters)
 titles = "Deg numerator, Deg denominator, Number of samples, Total sample space"
