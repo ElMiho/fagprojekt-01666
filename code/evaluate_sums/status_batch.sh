26a741f13543ec62764cbf13649f3e71683af934
ElMiho <mikael.hjermitslev@gmail.com>
2023-04-10 10:32:54 +0200

    Save total sum and total percentage in folder
    Also added status batch script

---
+++ code/evaluate_sums/status_batch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#BSUB -q hpc
+#BSUB -J status_script_random
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -W 72:00
+#BSUB -u s214753@dtu.dk
+#BSUB -B
+#BSUB -N
+#BSUB -o Output_%J.out
+#BSUB -e Output_%J.err
+
+while true
+do
+    ./status_script.sh random-answers-partition-8-4-2023
+    sleep 900
+done
--- code/evaluate_sums/status_script.sh
+++ code/evaluate_sums/status_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir status_folder
+
 output_folder=$1
 echo "Searching folder: " $output_folder
 list=("0-8" "0-9" "0-10" "1-3" "1-4" "1-5" "1-6" "1-7" "1-8" "1-9" "1-10" "2-4" "2-5" "2-6" "2-7" "2-8" "2-9" "2-10" "3-5" "3-6" "3-7" "3-8" "3-9" "3-10" "4-6" "4-7" "4-8" "4-9" "4-10" "5-7" "5-8" "5-9" "5-10" "6-8" "6-9" "6-10" "7-9" "7-10")
@@ -22,4 +24,6 @@ for i in "${list[@]}"; do
 done
 
 echo "Sum: " $sum
-echo "Percentage: " $(frac $sum $(mult $total $list_length))
+percentage=$(frac $sum $(mult $total $list_length))
+echo "Percentage: " $percentage
+echo $sum $percentage > status_folder/$(date +"%m-%d-%y-%T").txt
