42987320cb5eeeae287bef6f2737a618e0094d59
Jonas Larsen <jonas.larsen103@gmail.com>
2023-04-19 11:59:37 +0200

    validatio

--- code/validation/root_order.py
+++ code/validation/root_order.py
@@ -6,66 +6,28 @@ and numerator evaluates to the same sum.
 from validation.validation_medthods import random_list_of_nuerator_and_denominator
 from validation.validation_medthods import compare_a_list_of_equations_token
 from validation.validation_medthods import get_token_expressions
-from validation.validation_medthods import test_one_expression
-#from validation.validation_medthods import sup_number_of_expressions
+from validation.validation_medthods import sup_number_of_expressions
 
 from model.equation_interpreter import Equation
 
-import math
 import itertools 
 
 
 #%%
 
-def list_of_test_expressions_with_same_roots(int_roots_only: bool = False, spaceinterval: list = [-5,5], max_num: int = None, max_den: int = None):
+def list_of_test_expressions_with_same_roots():
     # tilføj et max så man max kan få input expressions
     
     
     # greate all options
-    num_roots, den_roots = random_list_of_nuerator_and_denominator(spaceinterval, False, int_roots_only)
-    
-    print(f"The random poly is:\n {num_roots} / {den_roots}")
-    
-    num_roots_len = len(num_roots)
-    den_roots_len = len(den_roots)
-        
-    x = list(itertools.permutations(num_roots))
-    y = list(itertools.permutations(den_roots))
-    
-    step_lenght_num = math.factorial(num_roots_len)//max_num
-    step_lenght_den = math.factorial(den_roots_len)//max_den
-    
-    
-    if max_num < math.factorial(num_roots_len) and step_lenght_num != 0:
-        x = x[0:(len(x)-1):step_lenght_num]
-    
-    if max_den < math.factorial(den_roots_len) and step_lenght_den != 0:
-        y = y[0:(len(x)-1):step_lenght_den]
-
-    
-    """
-    # gets all posible combinations of denominator and numerator
-    all_num_combinations = [list(p) for i,p in enumerate(x) if i%step_lenght_num == 0]
-    all_den_combinations = [list(p) for i,p in enumerate(y) if i%step_lenght_den == 0]
-    """
+    num_roots, den_roots = random_list_of_nuerator_and_denominator(False)
     
     # gets all posible combinations of denominator and numerator
-    print("calcutating all num combinations..")
-    all_num_combinations = [list(p) for p in x]
-    print("calcutating all den combinations..")
-    all_den_combinations = [list(p) for p in y]
+    all_num_combinations = [list(p) for p in itertools.permutations(num_roots)]
+    all_den_combinations = [list(p) for p in itertools.permutations(den_roots)]
     
-    """
-    #lav en liste med alle idx af all:num og all_den
-    len_num_com = [1 for _ in all_num_combinations]
-    len_den_com = [1 for _ in all_den_combinations]
-    """
-   
-    ## denne her der tager lang tid
-    print("combining the num and den")
-    combine = itertools.product(all_num_combinations, all_den_combinations)      
-    all_combinations = [list(p) for p in combine]    
-        
+    
+    all_combinations = [list(p) for p in itertools.product(all_num_combinations, all_den_combinations)]
     all_combinations[0][0].append("/")
     
     test_expressions = [list(itertools.chain(*p)) for p in all_combinations]
@@ -73,18 +35,17 @@ def list_of_test_expressions_with_same_roots(int_roots_only: bool = False, space
     return test_expressions
 
 
-x = list_of_test_expressions_with_same_roots(True, max_num = 10, max_den = 20)
+x = list_of_test_expressions_with_same_roots()
 
 
 #%%
 test_expressions = get_token_expressions(x)
 
+#%%
+print(test_expressions[0].getMathmetaicalNotation())
 
 #%%
-print(test_expressions[0].getMathemetaicalNotation())
-
-#%%
-res = compare_a_list_of_equations_token(x)
+res = compare_a_list_of_equations_token(test_expressions)
   
 #%%
     
--- code/validation/validation_medthods.py
+++ code/validation/validation_medthods.py
@@ -2,41 +2,34 @@
 Use this file for methods/functions that can be used in multiple validation test.
 '''
 
-import random
-import sys
 
-# SET PATH FOR OSX USERS
-if sys.platform == 'darwin':
-    sys.path.append('../code')
-    
-LOAD_MAIN_FLAG = True
-if LOAD_MAIN_FLAG:
-    from main import test_an_expression
-    
 from model.tokenize_input import token_input_space
 from model.tokenize_input import all_poly
 from model.equation_interpreter import Equation
-#from model.equation_interpreter import 
 
+from main import test_an_expression
 
-from validation.mathematica_from_python import input_to_lists
-from validation.mathematica_from_python import evaluate_sum
-from validation.mathematica_from_python import close_session
+import random
 
 
 
-#from data_analysis.int_data.generate_plot import parse_line
-
-
-def test_one_expression(test_expression, as_string: bool = True):
-    return test_an_expression(test_expression, not as_string)
 
 def get_token_expressions(test_expression: list):
     return [test_an_expression(i) for i in test_expression]
 
-
+def sup_number_of_expressions(max_ex: int, test_expression: list):
+    length = len(test_expression)
+    print(length)
+    if max_ex < length:
+        t_e = []
+        steps = length // max_ex
+        for i in range(0, steps-1):
+            print(i)
+            t_e.append(test_expression[i*steps])
+        return t_e
+    return test_expression
+    
 def compare_a_list_of_equations_token(equations: list):
-    # Vil hvike hvis man sammenligerne token række følgen men skal opdateres når vi kan sammenligne 2 quations med .getMathemetaicalNotation()
     n = len(equations)
     count = 0
     total = 0
@@ -52,7 +45,7 @@ def compare_a_list_of_equations_token(equations: list):
     
     return count/total
 
-def random_list_of_nuerator_and_denominator(spaceinterval: list = [-5,5] ,concatenate: bool = True, int_roots_only: bool = False):
+def random_list_of_nuerator_and_denominator(concatenate: bool = True):
     '''
     geneate a random sum to be evaluated
 
@@ -64,16 +57,10 @@ def random_list_of_nuerator_and_denominator(spaceinterval: list = [-5,5] ,concat
         all the denominator roots in a list
 
     '''
-    if (int_roots_only):
-        roots_num_kind = "numinator_int_only"
-        roots_den_kind = "dominator_int_only"
-    else :
-        roots_num_kind = "numinator_only"
-        roots_den_kind = "dominator_only"
     
     # denominator and numerator
-    numerator_roots = token_input_space(spaceinterval[0], spaceinterval[1], roots_num_kind)
-    denominator_roots = token_input_space(spaceinterval[0], spaceinterval[1], roots_den_kind)
+    numerator_roots = token_input_space(-5, 5, "numinator_only")
+    denominator_roots = token_input_space(-5, 5, "dominator_only")
     
     # all oder over oders, a list of lists
     poly_oder_list = all_poly(10)
@@ -97,66 +84,8 @@ def random_list_of_nuerator_and_denominator(spaceinterval: list = [-5,5] ,concat
     if concatenate:
         num_roots.append("/")
         
-        return [num_roots + den_roots]
+        return num_roots + den_roots
     
     
     return num_roots, den_roots
 
-
-
-def extend_sum(eq_list, space = [-5,5], int_roots_only = False):
-    '''Extends a sum in both denominator and numinator with a random number
-    (The sum remains the same).
-    '''
-    if len(eq_list) > 1:
-        eq_list = eq_list[0] +['/']+ eq_list[1]
-    index = eq_list.index('/')
-    all_eq = []
-    if int_roots_only:
-        random_number = token_input_space(space[0], space[1], "numinator_int_only")
-        
-    else:
-        random_number = token_input_space(space[0], space[1], "numinator_only")
-
-    for num in random_number:
-        all_eq.append(eq_list[:index] + [num] + eq_list[index:] + [num])
-    return all_eq
-
-
-def evaluate_tokenized_sum(test_expression: list):
-    
-    for expression in test_expression:
-        print(expression)
-        numerator_roots, denominator_roots = input_to_lists(expression)
-        print(evaluate_sum(numerator_roots, denominator_roots))
-    close_session()    
-
-'''        
-def find_10_simpelest_evaluations():
-    #færdig gør denne kode til at finde de simpelste svar (oversæt dem til tokens og tæl ikke int tokens og ral tokens)
-    with open("data_analysis/int-data/megafile.txt", "r") as file:
-        lines = file.readlines()
-    
-    for line in lines:
-        time, answer, sum_degree = parse_line(line)
-'''
-
-
-
-if __name__ == '__main__':
-    #evaluate_tokenized_sum(random_list_of_nuerator_and_denominator([-5,5], int_roots_only = True))
-    sums = random_list_of_nuerator_and_denominator([-5,5],int_roots_only=True)
-    sums2 = ["#", "/", "1/2", "1/5"]
-    sums = random_list_of_nuerator_and_denominator([-5,5],int_roots_only=False)
-    x = test_one_expression(sums2, False)
-    y = test_one_expression(sums2, True)
-    #[print(i) for i in x]
-    tt_list = x.listOfTokens()
-    [print(i) for i in tt_list]
-    
-    equation = Equation.makeEquationFromString("-Sin(2-EulerGamma)+a/3+(-7/3*2 + Pi^2)-2")
-    
-    tt_list2 = equation.listOfTokens()
-    tt_ = equation.tokenized_equation
-    
-  
