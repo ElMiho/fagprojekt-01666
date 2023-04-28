89357b681345df2bb5a43a23261470229b21fec1
ElMiho <mikael.hjermitslev@gmail.com>
2023-04-28 15:22:10 +0200

    Use functions instead

--- code/validation/correct_answer.py
+++ code/validation/correct_answer.py
@@ -1,7 +1,6 @@
 import sys
 import networkx as nx
 import matplotlib.pyplot as plt
-import random
 
 # SET PATH FOR OSX USERS
 if sys.platform == 'darwin':
@@ -9,84 +8,6 @@ if sys.platform == 'darwin':
 
 import model.tokenize_input as tokenize_input
 import model.equation_interpreter as equation_interpreter
-
-# Code from stackoverflow
-def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
-
-    '''
-    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.  
-    Licensed under Creative Commons Attribution-Share Alike 
-    
-    If the graph is a tree this will return the positions to plot this in a 
-    hierarchical layout.
-    
-    G: the graph (must be a tree)
-    
-    root: the root node of current branch 
-    - if the tree is directed and this is not given, 
-      the root will be found and used
-    - if the tree is directed and this is given, then 
-      the positions will be just for the descendants of this node.
-    - if the tree is undirected and not given, 
-      then a random choice will be used.
-    
-    width: horizontal space allocated for this branch - avoids overlap with other branches
-    
-    vert_gap: gap between levels of hierarchy
-    
-    vert_loc: vertical location of root
-    
-    xcenter: horizontal location of root
-    '''
-    if not nx.is_tree(G):
-        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
-
-    if root is None:
-        if isinstance(G, nx.DiGraph):
-            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
-        else:
-            root = random.choice(list(G.nodes))
-
-    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
-        '''
-        see hierarchy_pos docstring for most arguments
-
-        pos: a dict saying where all nodes go if they have been assigned
-        parent: parent of this branch. - only affects it if non-directed
-
-        '''
-    
-        if pos is None:
-            pos = {root:(xcenter,vert_loc)}
-        else:
-            pos[root] = (xcenter, vert_loc)
-        children = list(G.neighbors(root))
-        if not isinstance(G, nx.DiGraph) and parent is not None:
-            children.remove(parent)  
-        if len(children)!=0:
-            dx = width/len(children) 
-            nextx = xcenter - width/2 - dx/2
-            for child in children:
-                nextx += dx
-                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap, 
-                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
-                                    pos=pos, parent = root)
-        return pos
-
-            
-    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
-
-test_string = "Pi^2/6"
-eq = equation_interpreter.Equation.makeEquationFromString(test_string)
-print(eq.notation)
-eq.convertToPostfix()
-print(eq.notation)
-
-print(eq.tokenized_equation)
-print(len(eq.tokenized_equation))
-
-G = nx.Graph()
-# print(eq.tokenized_equation[-5])
 
 binary_operators = [
     "TT_PLUS",
@@ -96,26 +17,50 @@ binary_operators = [
     "TT_POW"
 ]
 
-idx = -1
-while idx >= -len(eq.tokenized_equation):
-    type = eq.tokenized_equation[idx].t_type
-    value = eq.tokenized_equation[idx].t_value
-    print(-idx, type, value)
+def equation_string_to_graph(input_string: str):
+    eq = equation_interpreter.Equation.makeEquationFromString(input_string)
+    eq.convertToPostfix()
+    G = nx.Graph()
 
-    G.add_node(-idx, symbol=type)
+    token_list = eq.tokenized_equation
 
-    if type in binary_operators:
-        G.add_edge(-idx, -(idx + 1))
-        G.add_edge(-idx, (-idx + 2))
+    for idx, node in enumerate(reversed(token_list)):
+        type = node.t_type
+        value = node.t_value
+        G.add_node(idx, symbol = type)
 
-    idx -= 1
-    
-print(G.nodes.data())
+    for idx, node in enumerate(reversed(token_list)):
+        type = node.t_type
+        value = node.t_value
 
-plt.figure(1)
-pos = nx.spring_layout(G)
-# pos = hierarchy_pos(G, 1)
-labels = nx.get_node_attributes(G, 'symbol')
-nx.draw(G, labels=labels)
-plt.show()
+        if type in binary_operators:
+            G.add_edge(idx, idx + 1)
+            G.add_edge(idx, idx + 2)
 
+    return G
+
+# Doesn't plot but rather saves the necessary commands
+def plot_graph(G):
+    pos = nx.spring_layout(G)
+    labels = nx.get_node_attributes(G, 'symbol')
+    nx.draw(G, labels=labels)
+
+
+if __name__ == '__main__':
+    test_string = "Pi^2/6"
+    predicted_test_string = "Pi^Z/Z + Pi"
+
+    plt.figure(1)
+    G = equation_string_to_graph(test_string)
+    plot_graph(G)
+
+
+    plt.figure(2)
+    J = equation_string_to_graph(predicted_test_string)
+    plot_graph(J)
+
+    print(nx.graph_edit_distance(
+        G, J
+    ))
+
+    plt.show()
