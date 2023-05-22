af303a4f9a6ab99b12469c8045d05174a5ecb33c
ElMiho <mikael.hjermitslev@gmail.com>
2023-05-22 08:46:51 +0200

    Move files from earlier sampling code

--- code/sampling/generate_sample_parameters.py
+++ code/sampling/generate_sample_parameters.py
@@ -13,8 +13,12 @@ def save_data(file_names):
     abortion_rate = []
     sample_size = []
 
-    for a,b in file_names:
-        input_file =  f"../data_generation/7_sec_data/data/answers-{a}-{b}.txt"
+    for string in file_names:
+        [a, b] = string.split("-")
+        a = int(a)
+        b = int(b)
+        # input_file =  f"../data_generation/7_sec_data/data/answers-{a}-{b}.txt"
+        input_file = f"./abortion_rate_int/{a}-{b}.txt"
         number_of_lines = sum(1 for i in open(input_file, 'rb'))
         num_non_zeros = number_of_lines
 
@@ -41,14 +45,34 @@ def save_data(file_names):
     sample_size = np.asarray(sample_size)
     return token_lengths,abortion_rate,sample_size
 
-file_names = np.loadtxt("file_names.txt")
+# file_names = np.loadtxt("file_names.txt")
+file_names = [
+    "0-2", "0-3", "0-4", "0-5", "0-6", "0-7", "0-8", "0-9", "0-10", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8", "1-9", "1-10", "2-4", "2-5", "2-6", "2-7", "2-8", "2-9", "2-10", "3-5", "3-6", "3-7", "3-8", "3-9", "3-10", "4-6", "4-7", "4-8", "4-9", "4-10", "5-7", "5-8", "5-9", "5-10", "6-8", "6-9", "6-10", "7-9", "7-10", "8-10"
+]
 
 token_lengths,abortion_rate,sample_size = save_data(file_names)
-
+print(f"test{sample_size}")
 #Export data as matlab file
 savemat('abort_rate_integer.mat',{'abortion_rate': abortion_rate})
 savemat('Token_Length_integer.mat',{'Token_len': token_lengths})
 savemat('Sample_size_integer.mat',{'sample_size': sample_size})
 
-#Save sample parameters as txt file
-np.savetxt('SampleSizeInteger.txt', sample_size, fmt='%i,%i,%.3f', delimiter=' ')
+# Generate 44x3 array with [Deg numerator, Deg denominator, Number of samples, Total sample space]
+
+sample_parameters = []
+
+number_of_tokens_num = 21
+number_of_tokens_den = 11
+total_samples = 40*10**6
+categories = 44
+n = total_samples//categories
+for i in range(categories):
+    num = sample_size[i,0]
+    den = sample_size[i,1]
+    sample_factor = sample_size[i,2]
+    number_of_samples = n*sample_factor
+    omega = number_of_tokens_num**num*number_of_tokens_den**den     #Change according to different bases
+    sample_parameters.append((num,den,number_of_samples,omega))
+sample_parameters = np.asarray(sample_parameters)
+titles = "Deg numerator, Deg denominator, Number of samples, Total sample space"
+np.savetxt('SampleParametersIntegers.txt',sample_parameters, fmt='%i', delimiter=',', header=titles)
---
+++ code/sampling/integer_sampling/rand_int.py
@@ -0,0 +1,114 @@
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import os
+import random
+import sympy as sp
+
+fraction_list_num = ["-10", "-9" , "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1","2", "3", "4", "5", "6" ,"7", "8", "9", "10"]
+fraction_list_den = ["-10", "-9" , "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0"]
+
+
+#Functions
+def int_tuple_list(n, deg_num, deg_den, base_num, base_den):
+    #Split into two unique integers using division with remainder by n and base_num**deg_num
+    #n_num = remainder, n_den = quotient
+    divisor = (base_num**deg_num)
+    quo, rem = divmod(n,divisor)
+    n_den = quo
+    n_num = rem
+    poly_fraction = []
+
+    print(f"n: {n}, ")
+
+    #Generate numerator and denominator independently
+    poly_num = integer_to_list(n_num, deg_num, base_num)
+    poly_den = integer_to_list(n_den, deg_den, base_den)
+    poly_fraction.extend((poly_num,poly_den))
+    poly_fraction_out = [item for sublist in poly_fraction for item in sublist]
+    return poly_fraction_out
+
+def integer_to_list(n, sum_deg, base):
+    # allocates index 0 and 1 to roots in numerator and denominator
+    l = []
+    for i in range(1, sum_deg+1):
+        var = n % base**i
+        l.append(var // (base**(i-1))+1)
+        n = n - var
+    l = np.asarray(l)
+    return l
+
+def list_to_roots(list_int, deg_num):
+    print(list_int)
+    print(deg_num)
+    fraction_list_num = ["-10", "-9" , "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1","2", "3", "4", "5", "6" ,"7", "8", "9", "10"]
+    fraction_list_den = ["-10", "-9" , "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0"]
+
+    list_num = []
+    list_den = []
+
+    idx_num = list_int[0:deg_num]
+    idx_den = list_int[deg_num:]
+    
+    for i in idx_num:
+        root = fraction_list_num[i-1]
+        list_num.append(root)
+    for i in idx_den:
+        root = fraction_list_den[i-1]
+        list_den.append(root)
+    
+    return list_num,list_den
+
+# Generate_folder for expressions
+folder_name_int = "random_expressions_integers"
+folder_name_roots = "random_expressions_roots"
+current_directory = os.getcwd()
+final_directory = os.path.join(current_directory, folder_name_int)
+if not os.path.exists(final_directory):
+   os.makedirs(final_directory)
+final_directory = os.path.join(current_directory, folder_name_roots)
+if not os.path.exists(final_directory):
+   os.makedirs(final_directory)
+
+#### Import sampling parameters
+#44x4 np-array [Deg numerator, Deg denominator, Number of samples, Total sample space]
+sample_parameters = np.loadtxt('SampleParametersIntegers.txt', delimiter=',')
+
+####  Initialize sampling
+base_num = 21
+base_den = 11
+#num_categories = sample_parameters.shape[0]    commented while testing
+num_categories = 44
+for i in range(num_categories):
+    print("current: " + str(i))
+    deg_num = int(sample_parameters[i,0])
+    deg_den = int(sample_parameters[i,1])
+    #sample_size = int(sample_parameters[i,2])  commented while testing
+    sample_size = 10                     
+    sample_space = int(sample_parameters[i,3])
+    
+    print("sample_space: " + str(sample_space))
+    print("sample_size: " + str(sample_size))
+    samples = [random.randint(1, sample_space) for _ in range(sample_size)]
+
+    #From integer to list and saves file in random expression folder
+    integer_expressions = []
+    root_list_den = []
+    root_list_num = []
+    sum_deg = deg_num + deg_den
+    for j in range(len(samples)):
+        n = samples[j]
+        list_int = int_tuple_list(n, deg_num, deg_den, base_num, base_den)
+        root_num,root_den = list_to_roots(list_int, deg_num)
+
+        root_list_num.append(root_num)
+        root_list_den.append(root_den)
+        integer_expressions.append(list_int)
+
+    root_list_den = np.asarray(root_list_den)
+    root_list_num = np.asarray(root_list_num)
+    integer_expressions = np.asarray(integer_expressions)
+    #Save txt. file    
+    np.savetxt(f'{folder_name_int}/rand_int_expressions-{deg_num}-{deg_den}.txt',integer_expressions, fmt='%i', delimiter=',')
+    np.savetxt(f'{folder_name_roots}/rand_roots_num_expressions-{deg_num}-{deg_den}.txt',root_list_num, fmt='%s', delimiter=',')
+    np.savetxt(f'{folder_name_roots}/rand_roots_den_expressions-{deg_num}-{deg_den}.txt',root_list_den, fmt='%s', delimiter=',')
