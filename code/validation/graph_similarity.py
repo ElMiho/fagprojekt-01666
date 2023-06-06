a2d962f624435bf6daf95839433f8f96b7c9fe14
ElMiho <mikael.hjermitslev@gmail.com>
2023-06-06 12:38:23 +0200

    Just to save the changes

--- code/validation/graph_similarity.py
+++ code/validation/graph_similarity.py
@@ -2,6 +2,9 @@ import networkx as nx
 import sys
 import matplotlib.pyplot as plt
 
+# algorithm already implemented in zss
+from zss import simple_distance, Node
+
 # SET PATH FOR OSX USERS
 if sys.platform == 'darwin':
     sys.path.append('../code')
@@ -19,6 +22,7 @@ G1.add_edge(0, 1)
 G1.add_edge(0, 2)
 G1.add_edge(2, 3)
 G1.add_edge(2, 4)
+
 
 ## pi^Z / Z + log(Z)
 G2 = nx.Graph()
@@ -53,6 +57,10 @@ def weisfeiler_leman_1D():
     pass
 
 def wiener_index(G: nx.Graph):
+    """
+    Input: A graph G
+    Output: Computes the Wiener index
+    """
     nodes = G1.nodes
     number_of_shortest_paths = 0
     sum = 0
@@ -64,10 +72,49 @@ def wiener_index(G: nx.Graph):
 
     return sum/number_of_shortest_paths
 
+# old
+def graph_edit_distance(G1: nx.Graph, G2: nx.Graph):
+    """
+    Inputs: Two graphs G1 and G2
+    Outputs: The steps to go from G1 to G2
+
+    Comments and notation:
+    (u -> v): u in G1.nodes replaces v in G2.nodes
+    (u -> eps): deletion of u on G1.nodes
+    (eps -> v): insertion of v in G2.nodes
+
+    all of these are modelled as tuples e.g. (u, v)
+    """
+    # INIT
+    V1 = G1.nodes
+    V2 = G2.nodes
+    open = set()
+    for w in range(len(V2)):
+        open.add((0, w))
+    open.add((1, "eps"))
+
+    print(f"open: {open}")
+
+## using zss
+A = (
+    Node("/")
+    .addkid(Node("Z"))
+    .addkid(Node("^")
+        .addkid(Node("pi"))
+        .addkid(Node("Z"))
+    )
+)
+    
+def insert(G: nx.Graph):
+    pass
 
 if __name__ == '__main__':
     print(f"Wiener index of G1: {wiener_index(G1)}")
     print(f"Wiener index of G2: {wiener_index(G2)}")
+
+    graph_edit_distance(G1, G2)
+
+    print(simple_distance(A, A))
 
     plt.figure(1)
     plot_graph(G1)
