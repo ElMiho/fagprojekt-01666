0e8fe69daa9c53edbfc4c810ffa6870d7fdfe7b5
Larsen00 <jonas.larsen103@gmail.com>
2023-06-08 10:53:14 +0200

    combi

--- code/validation/graphGeneration.py
+++ code/validation/graphGeneration.py
@@ -49,9 +49,7 @@ def generate_graph_from_postfix(tokens): #Tokens should be listed in postfix!
 
 
 def generate_graph_from_postfix2(tokens): #Tokens should be listed in postfix!
-    # DRAFT 
     stacks = [[]]
-    
     for token in tokens:
         if token.t_type not in binary_operators:
             for stack in stacks:
@@ -61,14 +59,20 @@ def generate_graph_from_postfix2(tokens): #Tokens should be listed in postfix!
                 stack = stacks[i]
                 operand2 = stack.pop()
                 operand1 = stack.pop()
-                stack_copy = stack
+                stack_copy = stack.copy()
                 stack.append(Node(token.t_type, [operand1, operand2]))
                 stacks[i] = stack
                 if token.t_type in plus_mulitply:
                     stack_copy.append(Node(token.t_type, [operand2, operand1]))
                     stacks.append(stack_copy)
-                    
-    return stacks
+    
+    graphs = []
+    for g in stacks:
+        graphs.append(g.pop())
+    
+    return graphs
+
+
 
 def generate_combinations(graph):
 
@@ -110,6 +114,7 @@ def getDistance(graph1, graph2):
     return smallest_distance
 
 #Test cases
+'''
 equation = equation_interpreter.Equation.makeEquationFromString("4+Pi/(3+2)")
 equation.convertToPostfix()
 tokens = equation.tokenized_equation
@@ -120,4 +125,26 @@ g = generate_graph_from_postfix(tokens)
 g1 = generate_graph_from_postfix(tokens1)
 
 dist = getDistance(g, g1)
+print(dist)
+'''
+
+equation = equation_interpreter.Equation.makeEquationFromString("4+Pi/(3+2)")
+equation.convertToPostfix()
+tokens = equation.tokenized_equation
+gs = generate_graph_from_postfix2(tokens)
+equation1 = equation_interpreter.Equation.makeEquationFromString("Pi/(2+3)+4")
+equation1.convertToPostfix()
+tokens1 = equation1.tokenized_equation
+g1 = generate_graph_from_postfix(tokens1)
+
+def getDistance2(tokens, graph2):
+    combs = generate_graph_from_postfix2(tokens)
+    smallest_distance = sys.maxsize
+    for c in combs:
+        distance = simple_distance(c, graph2)
+        if distance < smallest_distance:
+            smallest_distance = distance
+    return smallest_distance
+
+dist = getDistance2(tokens, g1)
 print(dist)
