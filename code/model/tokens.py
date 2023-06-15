c2f5a069c94596b687c49e228d823e7d96f833b0
Hugo Nielsen <hugomn2002@gmail.com>
2023-06-15 14:54:51 +0200

    Operators

--- code/model/tokens.py
+++ code/model/tokens.py
@@ -44,6 +44,51 @@ TT_MULTIPLY = "TT_MULTIPLY"
 TT_DIVIDE = "TT_DIVIDE"
 TT_POW = "TT_POW"
 
+unary_operators = {
+    TT_SQRT, TT_SIN, TT_COS, TT_TAN, TT_LOG
+}
+
+binary_operators = {
+    TT_PLUS, TT_MINUS, TT_MULTIPLY, TT_DIVIDE, TT_POW
+}
+
+commutative_operators = {
+    TT_PLUS, TT_MULTIPLY
+}
+
+# Token to latex mapper
+token2symbol = {
+    TT_LEFT_PARENTHESIS: "(",
+    TT_RIGHT_PARENTHESIS: ")",
+
+    ## Special numbers
+    TT_PI: r"\pi",
+    TT_E: "e",
+    TT_PHI: r"\varphi",
+    TT_CATALAN: "G",
+    TT_EULERGAMMA: r"\gamma", # i.e. the euler-mascheroni constant
+    TT_ZERO: "0",
+    TT_ONE: "1",
+
+    ## Numbers
+    TT_INTEGER: "Z",
+
+    ## Unary Operations
+    TT_SQRT: r"\sqrt{\;}",
+    TT_SIN: r"\sin",
+    TT_COS: r"\cos",
+    TT_TAN: r"\tan",
+    TT_LOG: r"\log",
+
+    ## Binary Operations
+    TT_PLUS: "+",
+    TT_MINUS: "-",
+    TT_MULTIPLY: r"\times",
+    TT_DIVIDE: "/",
+    TT_POW: r"^",
+    None: None
+}
+
 
 ## List of all token types
 TOKEN_TYPE_ANSWERS = [
@@ -67,4 +112,3 @@ TOKEN_TYPE_EXPRESSIONS = [str(v) for v in token_input_space(-5,5)]
 if __name__ == "__main__":
     print(f"TT ANSWERS: {TOKEN_TYPE_ANSWERS}")
     print(f"TT EXPRESSIONS: {TOKEN_TYPE_EXPRESSIONS}")
-
