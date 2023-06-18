4fd8461b4c9b7b5f59a036916dc317c7261686ec
Larsen00 <jonas.larsen103@gmail.com>
2023-06-18 23:44:21 +0200

    svae day

--- code/validation/plot_roots.py
+++ code/validation/plot_roots.py
@@ -22,14 +22,14 @@ for file in file_list:
         dis.append(data['found_distance'])
 
 
-zi = zip(dis[0], dis[1], dis[2], dis[3], dis[4])
-data = [row1 + row2 + row3 + row4+ row5 for row1, row2, row3, row4, row5 in zi]
+zi = zip(dis[0], dis[1], dis[2], dis[3], dis[4],dis[5],dis[6],dis[7])
+data = [row1 + row2 + row3 + row4+ row5 +row6 + row7 + row8 for row1, row2, row3, row4, row5, row6, row7, row8 in zi]
 
 degree_vector = posible_degrees(10)
 
 idx = 0
 labels = []
-p = [0, 1, 2,3,4]
+p = [0, 1, 2,3,4,5,6,7]
 data_sort = []
 non_empt = 0
 hist_data = []
@@ -46,7 +46,7 @@ for d in data:
     plt.scatter([non_empt]*len(data[idx]), data[idx], marker='o', label=label)
     idx += 1
     non_empt +=1
-    hist_data.append(len(d)/100)
+    hist_data.append(len(d))
     histdata.append(len(d))
 
    
@@ -54,18 +54,49 @@ for d in data:
 plt.boxplot(data_sort, positions=p, labels=labels)
 plt.ylabel('Distance')
 plt.xlabel('Degree of P(n) / Degree of Q(n)')
-plt.title('Distance between correct and predicted evaluations')
+plt.title('Average Distance when permuting random expressions')
 plt.xticks(rotation=-45)
 plt.show()
 
-def addlabels(x,y):
+def addlabels(x,y,data):
     for i in range(len(x)):
-        plt.text(i,y[i],histdata[i])
+        plt.text(i,y[i],data[i])
+
+zero_counts = [row.count(0.0) for row in data if len(row) != 0]
         
-plt.bar(labels, hist_data, color ='blue',width = 0.4)
-addlabels(labels, hist_data)
+plt.bar(labels, hist_data, color ='blue',width = 0.2)
+plt.bar(labels, zero_counts, color ='red',width = 0.2)
+addlabels(labels, hist_data, hist_data)
 plt.title('Percentage of valid notation \n in each category')
 plt.ylabel('Percentage valid Postfix Notation')
 plt.xlabel('Degree of P(n) / Degree of Q(n)')
 plt.show()
 
+#%%
+# Width of each bar
+bar_width = 0.35
+
+# Positions of the left bar-boundaries
+bar_positions = np.arange(len(labels))
+
+# Create the figure and axis objects
+fig, ax = plt.subplots()
+
+# Plot the first bar plot
+ax.bar(bar_positions, hist_data, width=bar_width, label='Valid postfix notation')
+
+# Plot the second bar plot next to the first one
+ax.bar(bar_positions + bar_width, zero_counts, width=bar_width, label='Average distance equal to 0')
+
+addlabels(labels, hist_data,hist_data)
+addlabels(labels, zero_counts,zero_counts)
+
+# Customize the plot
+ax.set_xticks(bar_positions + bar_width / 2)
+ax.set_xticklabels(labels)
+ax.legend()
+plt.title('Percentage of valid notation \n in each category')
+plt.ylabel('Percentage valid Postfix Notation')
+plt.xlabel('Degree of P(n) / Degree of Q(n)')
+# Display the plot
+plt.show()
--- code/validation/result_plot.py
+++ code/validation/result_plot.py
@@ -66,8 +66,7 @@ found_distance7 = loaded_variables7['found_distance']
 total_counter7 = loaded_variables7['total_counter']
 non_valid_counter7 = loaded_variables7['non_valid_counter']
 degree_vector7 = loaded_variables7['degree_vector']    
-
-with open(f"validation/save_data/saved_variables_2_4.pkl", 'rb') as f:
+with open(f"validation/save_data/saved_variables_1_5.pkl", 'rb') as f:
     loaded_variables8 = pickle.load(f)
 # Access the loaded variables
 found_distance8 = loaded_variables8['found_distance']
@@ -75,9 +74,15 @@ found_distance8 = loaded_variables8['found_distance']
 #non_valid_counter8 = loaded_variables8['non_valid_counter']
 #degree_vector8 = loaded_variables8['degree_vector']
 
+with open(f"validation/save_data/saved_variables_2_4.pkl", 'rb') as f:
+    loaded_variables9 = pickle.load(f)
+# Access the loaded variables
+found_distance9 = loaded_variables9['found_distance']
+
+
 #%%
-zi = zip(found_distance1, found_distance2, found_distance3, found_distance4, found_distance5, found_distance6, found_distance7, found_distance8)
-data = [row1 + row2 + row3 + row4+ row5+ row6+ row7+ row8 for row1, row2, row3, row4, row5, row6, row7, row8 in zi]
+zi = zip(found_distance1, found_distance2, found_distance3, found_distance4, found_distance5, found_distance6, found_distance7, found_distance8, found_distance9)
+data = [row1 + row2 + row3 + row4+ row5+ row6+ row7 + row8 + row9 for row1, row2, row3, row4, row5, row6, row7, row8, row9 in zi]
 
 
 #%%
@@ -85,7 +90,7 @@ data = [row1 + row2 + row3 + row4+ row5+ row6+ row7+ row8 for row1, row2, row3, 
 
 idx = 0
 labels = []
-p = [0, 1, 2,3,4,5,6,7]
+p = [0, 1, 2,3,4,5,6,7,8]
 data_sort = []
 non_empt = 0
 hist_data = []
@@ -102,7 +107,7 @@ for d in data:
     plt.scatter([non_empt]*len(data[idx]), data[idx], marker='o', label=label)
     idx += 1
     non_empt +=1
-    hist_data.append(len(d)/100)
+    hist_data.append(len(d))
     histdata.append(len(d))
 
    
@@ -126,4 +131,40 @@ plt.title('Percentage of valid notation \n in each category')
 plt.ylabel('Percentage valid Postfix Notation')
 plt.xlabel('Degree of P(n) / Degree of Q(n)')
 plt.show()
+
+#%%
+counts = [sum(1 for value in row if value <= 5) for row in data_sort]
+            
+            
+def addlabels(x,y,data):
+    for i in range(len(x)):
+        plt.text(i,y[i],data[i])
+# Width of each bar
+bar_width = 0.35
+
+# Positions of the left bar-boundaries
+bar_positions = np.arange(len(labels))
+
+# Create the figure and axis objects
+fig, ax = plt.subplots()
+
+# Plot the first bar plot
+ax.bar(bar_positions, hist_data, width=bar_width, label='Valid postfix notation')
+
+# Plot the second bar plot next to the first one
+ax.bar(bar_positions + bar_width, counts, width=bar_width, label='Average distance less than 6')
+
+addlabels(labels, hist_data,hist_data)
+addlabels(labels, counts,counts)
+
+# Customize the plot
+ax.set_xticks(bar_positions + bar_width / 2)
+ax.set_xticklabels(labels)
+ax.legend()
+plt.title('Distance')
+plt.ylabel('Percentage valid Postfix Notation')
+plt.xlabel('Degree of P(n) / Degree of Q(n)')
+# Display the plot
+plt.show()
+
 
